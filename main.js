bed285be651ecd2d1066754a562f1cc0e106c02e
SreedharNambi <Sreedhar.nambi.20@mountblue.tech>
2022-06-05 23:02:41 +0530

    3rd scenario

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let csvToJson=require("convert-csv-to-json");
 const matchesData=csvToJson.fieldDelimiter(',').getJsonFromCsv('iplDataSet/matches.csv');
+const deliveriesData=csvToJson.fieldDelimiter(',').getJsonFromCsv('iplDataSet/deliveries.csv');
 
 matchesPlayedPerEachYear(matchesData);
 numberOfMatchesWonByEachTeam(matchesData);
+numberOfRunsConcededByEachTeamIn2016(matchesData,deliveriesData);
 
 function matchesPlayedPerEachYear(inputData){
     let matchesPlayedInEachYear={};
@@ -36,7 +38,32 @@ function numberOfMatchesWonByEachTeam(inputData){
     console.log("Number of matches won by each team over all years :")
     console.log(numberOfMatchesWonByEachTeamOverAllYears);
 }
-matchesPlayedPerEachYear(matchesData);
-numberOfMatchesWonByEachTeam(matchesData);
 
+function numberOfRunsConcededByEachTeamIn2016(inputData1,inputData2){
+    let reqYear='2016';
+    let reqYearMatchIds=[];
+    let numberOfRunsConcededByEachTeamIn2016={};
+    for (let obj of inputData1){
+        let year=obj.season;
+        let matchId=obj.id;
+        if(reqYear==year){
+            reqYearMatchIds.push(matchId);
+        }
+    }
+     console.log(reqYearMatchIds.length);
+     let count=0;
+    for(let record of inputData2){
+        count+=1;
+        // let deliveryMatchId=record.match_id;
+        // let bowlingTeam=record.bowling_team;
+        // let runsConceded=record.extra_runs;
+        if(record.match_id in reqYearMatchIds){
+            let counter=record.bowling_team in numberOfRunsConcededByEachTeamIn2016?numberOfRunsConcededByEachTeamIn2016[record.bowling_team]:0;
+            numberOfRunsConcededByEachTeamIn2016[record.bowling_team]=counter+Number(record.extra_runs);
+
+        }
+    }
+    console.log(numberOfRunsConcededByEachTeamIn2016);
+    console.log(count);
+}
 
